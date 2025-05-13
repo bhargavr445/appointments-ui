cb0cbe3ba4791793b84e3285ec1fa7862987c92e
Bhargav R G <bhargavr445@gmail.com>
2025-05-12 22:30:22 -0400

    admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ export const routes: Routes = [
     { path: '', redirectTo: 'schedule', pathMatch: 'full' },
     { path: 'schedule', component: ScheduleAppointmentComponent },
     {
-        path: 'admin', loadComponent: () => import('./admin1/admin.component').then(c => c.AdminComponent), children: [
+        path: 'admin', loadComponent: () => import('./admin/admin.component').then(c => c.AdminComponent), children: [
             { path: '', redirectTo: 'date-search', pathMatch: 'full' },
-            { path: 'email-search', loadComponent: () => import('./admin1/email-search/email-search.component').then(c => c.EmailSearchComponent) },
-            { path: 'date-search', loadComponent: () => import('./admin1/date-search/date-search.component').then(c => c.DateSearchComponent) },
+            { path: 'email-search', loadComponent: () => import('./admin/email-search/email-search.component').then(c => c.EmailSearchComponent) },
+            { path: 'date-search', loadComponent: () => import('./admin/date-search/date-search.component').then(c => c.DateSearchComponent) },
         ]
     }
 ];
