354ed50cfcfba9b9a8221a0f17d34bb544475ab9
Bhargav R G <bhargavr445@gmail.com>
2025-08-10 13:40:51 -0400

    updated build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withDebugTracing, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withDebugTracing, withHashLocation, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appInterceptor } from './app.interceptor';
@@ -7,7 +7,7 @@ import { appInterceptor } from './app.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({scrollPositionRestoration: 'enabled'})),
+    provideRouter(routes, withHashLocation(), withComponentInputBinding(), withInMemoryScrolling({scrollPositionRestoration: 'enabled'})),
     provideHttpClient(withInterceptors([appInterceptor]))
   ]
 };
